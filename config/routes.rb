f533d16141227154b6594781653c510d2a200824
元木嵩人 <kmnernofkingdeep@gmail.com>
2017-12-31 21:15:08 +0900

    Second commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   root to: "hello#index"
+
+  #Tweetの投稿に関するページ
+  #投稿ページ表示
+  get "/tweets/new" => "tweets#new"
+  #投稿し、ホームに戻る
+  post "/tweets/new" => "tweets#create"
+  #投稿一覧
+  get "/tweets/index" => "tweets#index"
 end
